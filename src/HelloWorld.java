d29632c9a921649eb4a81f5330ec4f3a5148c413
Jonas Hansert <jonas.hansert@h-ka.de>
2022-12-06 10:51:29 +0100

    exceptions

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,24 +2,16 @@ import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        Karte karte1 = new HalliGalli(3, 5, "rot");
-        Karte karte2 = new Uno("blau", "+2");
-        Karte[] karten = {karte1, karte2};
-        for (int i = 0; i < karten.length; i++) {
-            karten[i].spielen();
+        Scanner sc = new Scanner(System.in);
+
+        String zahl = sc.nextLine();
+
+        try {
+            int z = Integer.parseInt(zahl);
+            System.out.println(z);
+        } catch (NumberFormatException e) {
+            System.out.println("Bitte eine Zahl eingeben!");
         }
-        System.out.println(add(1.5,2.3));
-    }
 
-    public static int add(int a, int b) {
-        return a+b;
-    }
-
-    public static int add(int a, int b, int c) {
-        return a+b+c;
-    }
-
-    public static double add(double a, double b) {
-        return a+b;
     }
 }
